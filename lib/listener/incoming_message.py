09b27d1f9617c06693429e2b9f068bb6aca1038f
Frans <franssiswanto@gmail.com>
2016-09-17 20:19:51 +0700

    test

--- lib/listener/incoming_message.py
+++ lib/listener/incoming_message.py
@@ -1,6 +1,11 @@
 import falcon
+import json
 
 class IncomingMessage:
 	def on_post(self, req, res):
-		print(req.body)
+		body = req.stream.read()
+		if not body:
+			raise falcon.HTTPBadRequest("Empty Request Body", "A valid JSON document is required.")
+		body = json.loads(body.decode("utf-8"))
+		print(json.dumps(body,indent=4))
 		res.status = falcon.HTTP_200
