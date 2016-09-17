bae4edaa8bc4c9bb9138bcfceda09a99ec3d77fc
Frans <franssiswanto@gmail.com>
2016-09-17 20:30:13 +0700

    test

--- lib/listener/incoming_message.py
+++ lib/listener/incoming_message.py
@@ -1,5 +1,7 @@
+import requests
 import falcon
 import json
+import shutil
 
 class IncomingMessage:
 	def on_post(self, req, res):
@@ -7,5 +9,13 @@ class IncomingMessage:
 		if not body:
 			raise falcon.HTTPBadRequest("Empty Request Body", "A valid JSON document is required.")
 		body = json.loads(body.decode("utf-8"))
-		print(json.dumps(body,indent=4))
+
+		if "mediaUrl" in body["messages"][0]:
+			media_url = body["messages"][0]["mediaUrl"]
+			r = requests.get(media_url, stream=True)
+			if r.status_code == 200:
+				with open(os.path.join(os.getcwd(),"images.jpg"), "wb") as f:
+					r.raw.decode_content = True
+					shutil.copyfileobj(r.raw,f)
+
 		res.status = falcon.HTTP_200
